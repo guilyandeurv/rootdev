0cbecd4febca74f00f9260567404b6c4adebeca7
GuiLyan Deurv <guilyandeurv@gmail.com>
2024-04-14 17:38:13 +0200

    Suppression du view image

--- content/.vitepress/theme/index.js
+++ content/.vitepress/theme/index.js
@@ -2,10 +2,6 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import 'viewerjs/dist/viewer.min.css';
-import imageViewer from 'vitepress-plugin-image-viewer';
-import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import { useRoute } from 'vitepress';
 
 
 /** @type {import('vitepress').Theme} */
@@ -18,18 +14,6 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
     //...
-  },
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx);
-    // Register global components, if you don't want to use it, you don't need to add it
-    ctx.app.component('vImageViewer', vImageViewer);
-    // ...
-  },
-  setup() {
-    // Get route
-    const route = useRoute();
-    // Using
-    imageViewer(route);
   }
 }
 
