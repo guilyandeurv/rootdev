5703e9e88612a3b4fcfb00cfb7bfcbb25f98c100
GuiLyan <guilyandeurv@gmail.com>
2024-10-24 18:58:34 +0200

    Bug sass deprecation résolu

--- content/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ content/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -273,8 +273,8 @@ import {
   watchTriggerable,
   watchWithFilter,
   whenever
-} from "./chunk-6E2JFA6K.js";
-import "./chunk-L2JNJ22P.js";
+} from "./chunk-R4W24RKT.js";
+import "./chunk-2V3V7MYD.js";
 export {
   DefaultMagicKeysAliasMap,
   StorageSerializers,
